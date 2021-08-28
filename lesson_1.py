1bcf5d45202bf132ee4389f58fe91c05c18ea4b0
Cameron <cjpuffnstuff@gmail.com>
2021-08-28 10:39:14 -0700

    Finish lesson one because jake told me to

--- lesson_1.py
+++ lesson_1.py
@@ -6,3 +6,42 @@
 	# Rookie Year (named rookie_year)
 	# Is Active (named is_active)
 # Be sure to store each variable as the correct type!
+
+def parse_input(inpt):
+    if (inpt == "y"):
+        return True
+    elif (inpt == "n"):
+        return False
+    else:
+        print("Unknown input " + inpt + " interperating as no")
+        return False
+
+
+val = input("Team number: ");
+print(val);
+
+try:
+    team_number = int(val);
+    print("Accepted");
+except ValueError:
+    team_number = 0;
+    print("Invalid team number");
+
+val = input("Team name: ");
+print(val);
+team_name = val
+
+val = input("Team location: ");
+print(val);
+location = val
+
+val = input("Rookie year [y/n]:");
+print(val);
+rookie_year = parse_input(val);
+
+val = input("Is Active [y/n]:");
+print(val);
+is_active = parse_input(val);
+
+print(str(team_number) + ", " + team_name + ", " + location + ", " + str(rookie_year) + ", " + str(is_active))
+
